a961ebd15b093d27451c99de9c2d70e739a24fd8
rampant-teeth <testDummyiez1@gmail.com>
2019-06-11 09:50:53 -0700

    home has listview

--- app/src/main/java/com/example/buzzerbets/AccountFragment.java
+++ app/src/main/java/com/example/buzzerbets/AccountFragment.java
@@ -8,40 +8,82 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
 public class AccountFragment extends Fragment {
+    int[] images = {R.drawable.steph_curry,
+                    R.drawable.steph_curry
+                    };
+    String[] accountItems = {"Item1",
+                                "Item2"
+                    };
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //return super.onCreateView(inflater, container, savedInstanceState);
+//        //return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_account, null);
-        String[] accountItems = {"Do something!",
-                                "Do something else!",
-                                "Do yet another thing!",
-                "Do something!",
-                "Do something else!",
-                "Do something!",
-                "Do something else!",
-                "Do something!",
-                "Do something else!",
-                "Do something else!",
-                "Do something!",
-                "Do something else!",
-                "Do something!",
-                "Do something else!",
-                "Do something!",
-                "Do something else!"};
         ListView listView = (ListView) view.findViewById(R.id.account);
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-            getActivity(),
-            android.R.layout.simple_list_item_1,
-            accountItems
-            );
-        listView.setAdapter(listViewAdapter);
-        //return inflater.inflate(R.layout.fragment_account, null);
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+//        String[] accountItems = {"Do something!",
+//                                "Do something else!",
+//                                "Do yet another thing!",
+//                "Do something!",
+//                "Do something else!",
+//                "Do something!",
+//                "Do something else!",
+//                "Do something!",
+//                "Do something else!",
+//                "Do something else!",
+//                "Do something!",
+//                "Do something else!",
+//                "Do something!",
+//                "Do something else!",
+//                "Do something!",
+//                "Do something else!"};
+//        ListView listView = (ListView) view.findViewById(R.id.account);
+//        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+//            getActivity(),
+//            android.R.layout.simple_list_item_1,
+//            accountItems
+//            );
+//        listView.setAdapter(listViewAdapter);
+//        //return inflater.inflate(R.layout.fragment_account, null);
         return view;
+    }
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.customlayout, null);
+            ImageView mImageView = view.findViewById(R.id.imageView);
+            TextView mTextView = view.findViewById(R.id.textview);
+            mImageView.setImageResource(images[position]);
+            mTextView.setText(accountItems[position]);
+            return view;
+        }
     }
 }
--- app/src/main/java/com/example/buzzerbets/HomeFragment.java
+++ app/src/main/java/com/example/buzzerbets/HomeFragment.java
@@ -1,3 +1,21 @@
+//package com.example.buzzerbets;
+//
+//import android.os.Bundle;
+//import android.support.annotation.NonNull;
+//import android.support.annotation.Nullable;
+//import android.support.v4.app.Fragment;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//public class HomeFragment extends Fragment {
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        //return super.onCreateView(inflater, container, savedInstanceState);
+//        return inflater.inflate(R.layout.fragment_home, null);
+//    }
+//}
 package com.example.buzzerbets;
 
 import android.os.Bundle;
@@ -7,12 +25,87 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+    int[] images = {R.drawable.steph_curry,
+            R.drawable.steph_curry,
+            R.drawable.steph_curry,
+            R.drawable.steph_curry,
+            R.drawable.steph_curry,
+            R.drawable.steph_curry
+    };
+    String[] homeItems = {"Item1",
+            "Item2", "Item3", "Item4", "Item5", "Item6"
+    };
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_home, null);
+//        //return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_home, null);
+        ListView listView = (ListView) view.findViewById(R.id.home);
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+//        String[] accountItems = {"Do something!",
+//                                "Do something else!",
+//                                "Do yet another thing!",
+//                "Do something!",
+//                "Do something else!",
+//                "Do something!",
+//                "Do something else!",
+//                "Do something!",
+//                "Do something else!",
+//                "Do something else!",
+//                "Do something!",
+//                "Do something else!",
+//                "Do something!",
+//                "Do something else!",
+//                "Do something!",
+//                "Do something else!"};
+//        ListView listView = (ListView) view.findViewById(R.id.account);
+//        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+//            getActivity(),
+//            android.R.layout.simple_list_item_1,
+//            accountItems
+//            );
+//        listView.setAdapter(listViewAdapter);
+//        //return inflater.inflate(R.layout.fragment_account, null);
+        return view;
+    }
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.customlayout, null);
+            ImageView mImageView = view.findViewById(R.id.imageView);
+            TextView mTextView = view.findViewById(R.id.textview);
+            mImageView.setImageResource(images[position]);
+            mTextView.setText(homeItems[position]);
+            return view;
+        }
     }
 }
